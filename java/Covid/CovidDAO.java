57bc9c6c59ffbc033770d9697d8066108673a691
Choi-y <67617801+Choi-y@users.noreply.github.com>
2022-03-07 14:54:52 +0900

    Update CovidDAO.java

--- java/Covid/CovidDAO.java
+++ java/Covid/CovidDAO.java
@@ -15,7 +15,7 @@ public class CovidDAO {
         String url = "https://www.busan.go.kr/covid19/Prevention06.do";
         Document doc = null;
         Elements tmp;
-        String gu = "부산진구";
+        String gu = "동래";
         Timestamp time = null;
         String num = null;
         int index = 0;
